d97305578980ce54763f2e307f3a3385fef87b40
Onur Bahtiyar <onrbahtiyar@gmail.com>
2024-08-20 23:49:29 +0300

    The console outputs have been updated, and table creation has been added.
    Kullanıcıya daha net geri bildirim sağlamak için konsol çıktıları
    güncellendi. Başarılı sonuçlar için "[✓]", başarısız sonuçlar için
    "[X]" ve hata durumunda "[ERROR]" eklendi. Ayrıca, veritabanı
    tablolarının oluşturulması için CreateTables yöntemi eklendi.

--- StarkSetup/Program.cs
+++ StarkSetup/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Enigma;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
 
@@ -92,14 +91,14 @@ internal class Program
             if (TestConnectionString(connectionString))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Connection successful!");
+                Console.WriteLine("[✓] Connection successful!");
                 Console.ResetColor();
                 break;
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Connection failed. Please enter a valid ConnectionString.");
+                Console.WriteLine("[X] Connection failed. Please enter a valid ConnectionString.");
                 Console.ResetColor();
             }
         }
@@ -128,14 +127,14 @@ internal class Program
                 if (IsValidAesKey(aesKey) && IsValidAesIV(aesIV))
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("AES Key and IV are valid.");
+                    Console.WriteLine("[✓] AES Key and IV are valid.");
                     Console.ResetColor();
                     break;
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid AES Key or IV. Please enter valid values.");
+                    Console.WriteLine("[X] Invalid AES Key or IV. Please enter valid values.");
                     Console.ResetColor();
                 }
             }
@@ -152,8 +151,8 @@ internal class Program
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("AES Key: " + aesKey);
-                Console.WriteLine("AES IV: " + aesIV);
+                Console.WriteLine("[✓] AES Key: " + aesKey);
+                Console.WriteLine("[✓] AES IV: " + aesIV);
                 Console.ResetColor();
 
                 isAesGenerated = true;
@@ -180,14 +179,14 @@ internal class Program
                 if (IsValidTokenKey(tokenKey))
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Token Key is valid.");
+                    Console.WriteLine("[✓] Token Key is valid.");
                     Console.ResetColor();
                     break;
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid Token Key. Please enter a valid Token Key. The key must be at least 64 bytes (88 Base64 characters) long.");
+                    Console.WriteLine("[X] Invalid Token Key. Please enter a valid Token Key. The key must be at least 64 bytes (88 Base64 characters) long.");
                     Console.ResetColor();
                 }
             }
@@ -205,7 +204,7 @@ internal class Program
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Token Key: " + tokenKey);
+                Console.WriteLine("[✓] Token Key: " + tokenKey);
                 Console.ResetColor();
 
                 isTokenGenerated = true;
@@ -228,21 +227,42 @@ internal class Program
         Console.ResetColor();
         string projectName = Console.ReadLine();
 
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Do you want to create system and user tables? (yes/no): ");
+        Console.ResetColor();
+        string createTables = Console.ReadLine()?.Trim().ToLower();
+
+        if (createTables == "yes")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[✓] Creating tables...");
+            Console.ResetColor();
+
+            CreateTables(connectionString);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[X] Tables were not created.");
+            Console.ResetColor();
+        }
+
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("\n-----------------------------------");
         Console.WriteLine("Summary:");
-        Console.WriteLine("ConnectionString: " + connectionString);
-        Console.WriteLine("AES Key: " + aesKey);
-        Console.WriteLine("AES IV: " + aesIV);
-        Console.WriteLine("Token Key: " + tokenKey);
-        Console.WriteLine("Project Name: " + projectName);
+        Console.WriteLine("[✓] ConnectionString: " + connectionString);
+        Console.WriteLine("[✓] AES Key: " + aesKey);
+        Console.WriteLine("[✓] AES IV: " + aesIV);
+        Console.WriteLine("[✓] Token Key: " + tokenKey);
+        Console.WriteLine("[✓] Project Name: " + projectName);
         Console.WriteLine("-----------------------------------");
         Console.ResetColor();
 
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("\nSetup completed successfully!");
+        Console.WriteLine("\n[✓] Setup completed successfully!");
         Console.ResetColor();
 
+        // appsettings.json dosyasını güncelle
         UpdateAppSettings(connectionString, aesKey, aesIV, projectName, tokenKey);
     }
 
@@ -311,7 +331,7 @@ internal class Program
         if (!File.Exists(webApiPath))
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"appsettings.json file not found: {webApiPath}");
+            Console.WriteLine($"[X] appsettings.json file not found: {webApiPath}");
             Console.ResetColor();
             return;
         }
@@ -353,18 +373,180 @@ internal class Program
         else
         {
             jObject["ProjectSettings"] = new JObject
-        {
-            { "ProjectName", projectName },
-            { "ProjectVersion", "1.0.0" }
-        };
+            {
+                { "ProjectName", projectName },
+                { "ProjectVersion", "1.0.0" }
+            };
         }
 
         File.WriteAllText(webApiPath, jObject.ToString());
 
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("\nappsettings.json file updated successfully!");
+        Console.WriteLine("\n[✓] appsettings.json file updated successfully!");
         Console.ResetColor();
     }
+
+    private static void CreateTables(string connectionString)
+    {
+        string createTablesSql = @"
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create the Role table
+CREATE TABLE [dbo].[Role](
+  [RoleId] [int] IDENTITY(1,1) NOT NULL,
+  [RoleName] [nvarchar](50) NOT NULL,
+  [Description] [nvarchar](250) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Role] ADD PRIMARY KEY CLUSTERED 
+(
+  [RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+-- Create the User table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Guid] [uniqueidentifier] NOT NULL,
+  [Email] [nvarchar](75) NULL,
+  [Password] [nvarchar](1000) NULL,
+  [FirstName] [nvarchar](75) NULL,
+  [LastName] [nvarchar](75) NULL,
+  [Username] [nvarchar](75) NULL,
+  [PhoneNumber] [nvarchar](50) NULL,
+  [DateOfBirth] [date] NULL,
+  [CreatedDate] [datetime] NULL,
+  [LastLoginDate] [datetime] NULL,
+  [IsEmailConfirmed] [bit] NULL,
+  [IsActive] [bit] NULL,
+  [ProfilePictureUrl] [nvarchar](350) NULL,
+  [Gender] [bit] NULL,
+  [Address] [nvarchar](250) NOT NULL,
+  [Country] [nvarchar](50) NULL,
+  [PreferredLanguage] [nvarchar](10) NULL,
+  [TwoFactorEnabled] [bit] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [UQ_User_Guid] UNIQUE NONCLUSTERED 
+(
+  [Guid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+-- Create the UserRole table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserRole](
+  [UserRoleId] [int] IDENTITY(1,1) NOT NULL,
+  [UserGuid] [uniqueidentifier] NOT NULL,
+  [RoleId] [int] NOT NULL,
+  [AssignedDate] [datetime] NOT NULL,
+  [UserId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[UserRole] ADD PRIMARY KEY CLUSTERED 
+(
+  [UserRoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[UserRole] ADD DEFAULT (getdate()) FOR [AssignedDate]
+GO
+-- WITH CHECK ifadesini kaldırıyoruz
+ALTER TABLE [dbo].[UserRole] ADD CONSTRAINT [FK_UserRole_Role] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Role] ([RoleId])
+GO
+-- WITH CHECK ifadesini kaldırıyoruz
+ALTER TABLE [dbo].[UserRole] ADD CONSTRAINT [FK_UserRole_UserGuid] FOREIGN KEY([UserGuid])
+REFERENCES [dbo].[User] ([Guid])
+GO
+-- Create the ErrorLog table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ErrorLog](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [UserGuid] [uniqueidentifier] NOT NULL,
+  [ActionName] [nvarchar](150) NULL,
+  [ControllerName] [nvarchar](150) NULL,
+  [Message] [nvarchar](max) NULL,
+  [StackTrace] [nvarchar](max) NULL,
+  [UserInput] [nvarchar](max) NULL,
+  [DateCreated] [datetime] NULL,
+  [System] [nvarchar](50) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ErrorLog] ADD CONSTRAINT [PK_ErrorLog] PRIMARY KEY CLUSTERED 
+(
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+-- Create the UserActivityLog table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserActivityLog](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [UserGuid] [uniqueidentifier] NULL,
+  [ActivityType] [nvarchar](max) NULL,
+  [ActivityDetail] [nvarchar](max) NULL,
+  [IPAddress] [nvarchar](max) NULL,
+  [BrowserInfo] [nvarchar](max) NULL,
+  [ActivityDate] [datetime] NULL,
+  [AdditionalData] [nvarchar](max) NULL,
+  [ActivityPage] [nvarchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[UserActivityLog] ADD CONSTRAINT [PK_UserActivityLog] PRIMARY KEY CLUSTERED 
+(
+  [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+        ";
+
+        try
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var commands = createTablesSql.Split(new string[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var command in commands)
+                {
+                    if (!string.IsNullOrWhiteSpace(command))
+                    {
+                        using (var sqlCommand = new SqlCommand(command, connection))
+                        {
+                            sqlCommand.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[✓] Tables created successfully!");
+            Console.ResetColor();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERROR] Failed to create tables: {ex.Message}");
+            Console.ResetColor();
+        }
+    }
+
 
     private static string EncryptData(string plainText)
     {
